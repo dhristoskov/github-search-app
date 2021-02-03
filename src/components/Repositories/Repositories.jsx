777627ea47641f4c2755b0565ccf8efd7af37e8b
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2021-02-03 19:06:27 +0100

    github-search-app add loader

--- src/components/Repositories/Repositories.jsx
+++ src/components/Repositories/Repositories.jsx
@@ -5,9 +5,23 @@ import './Repositories.scss'
 const Repositories = (props) => {
 
     console.log(props.repos);
-    
+
     return (
-        <div>Repositories</div>
+        <div>
+            {
+                props.repos.map(repo => {
+                    const { name, language, html_url, forks_url} = repo
+                    return (
+                        <div key={repo.id}>
+                            <p>{name}</p>
+                            <p>{language}</p>
+                            <p>{html_url}</p>
+                            <p>{forks_url}</p>
+                        </div>
+                    )  
+                })
+            }
+        </div>
     )
 }
 
