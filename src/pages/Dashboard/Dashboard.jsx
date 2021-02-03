5c5f05ec8ea135b4882ef78d2a5c1156057d82ad
Dimitar Hristoskov <d_hristoskov@hotmail.com>
2021-02-03 18:00:15 +0100

    github-search-app minor changes

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import { GitHubContext } from '../../context/GithubContext';
 import UserUpperSection from '../../components/UserUpperSection/UserUpperSection';
 import UserLowerSectio from '../../components/UserLowerSection/UserLowerSection';
 import SerachUser from '../../components/SearchUser/SearchUser';
-import Layout from '../../components/Layout/Layout';
+import Layout from '../../components/shared-components/Layout/Layout';
 import Repositories from '../../components/Repositories/Repositories';
 
 const Dashboard = () => {
